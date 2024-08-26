66161cc12e05880ae0ddc0b626713fbd3e42c725
Tofarati262 <oolof073@uOttawa.ca>
2024-08-25 22:32:07 -0300

    Finmor radio V1.74

--- client/src/Radio App/ProfileDrop.js
+++ client/src/Radio App/ProfileDrop.js
@@ -3,13 +3,34 @@ import "./ProfileDrop.css";
 import { ProfilePicContext } from './components/context/picContext';
 function ProfileDrop() {
     const {drop,setDrop}=useContext(ProfilePicContext);
+    const handleLogout = async () => {
+        console.log("trying");
+        try {
+          const response = await fetch("http://localhost:5000/logout/logout", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+    
+          if (response.ok) {
+            console.log("Successfully logged out");
+            window.location.href = "http://localhost:3000/";
+            // Perform any additional actions on successful logout, like redirecting the user
+          } else {
+            console.error("Failed to log out");
+          }
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      };
   return (
     <  >
 {    drop&&( <div className="List">
     <div className='Profile' >Profile</div> {/* Allows users to change the profile picture and theme */}
     <div className='Account' >Account</div>  {/* Allows users to change to premium or change to artist account  */}
     <div className="Upload" onClick={()=>window.location.href="http://localhost:3000/Upload"}>Upload</div>
-    <div className='Logout'>Logout</div> {/*Allow users to logout of their account !remember to add and test jwt functionality */}
+    <div className='Logout' onClick={handleLogout}>Logout</div> {/*Allow users to logout of their account !remember to add and test jwt functionality */}
   </div>)
 }   
   </>
